cf657be300978a1f24d59e62c7f4e2dda8b934ca
Abhishek Sharma <sabhiron5@gmail.com>
2022-05-17 00:03:54 +0530

    ..

---
+++ build/src/invoker/CMakeFiles/matsya-invoker.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/tokyo/Documents/GitHub/appmotor/src/common/report.c" "/home/tokyo/Documents/GitHub/appmotor/build/src/invoker/CMakeFiles/matsya-invoker.dir/__/common/report.c.o"
+  "/home/tokyo/Documents/GitHub/appmotor/src/invoker/invokelib.c" "/home/tokyo/Documents/GitHub/appmotor/build/src/invoker/CMakeFiles/matsya-invoker.dir/invokelib.c.o"
+  "/home/tokyo/Documents/GitHub/appmotor/src/invoker/invoker.c" "/home/tokyo/Documents/GitHub/appmotor/build/src/invoker/CMakeFiles/matsya-invoker.dir/invoker.c.o"
+  "/home/tokyo/Documents/GitHub/appmotor/src/invoker/search.c" "/home/tokyo/Documents/GitHub/appmotor/build/src/invoker/CMakeFiles/matsya-invoker.dir/search.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "DEBUG_LOGGING_DISABLED"
+  "PROG_NAME_INVOKER=\"matsya-invoker\""
+  "PROG_NAME_SINGLE_INSTANCE=\"matsya-single-instance\""
+  "SINGLE_INSTANCE_PATH=\"/usr/bin/matsya-single-instance\""
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src/invoker"
+  "/usr/include/dbus-1.0"
+  "/usr/lib/x86_64-linux-gnu/dbus-1.0/include"
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  "../src/common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
